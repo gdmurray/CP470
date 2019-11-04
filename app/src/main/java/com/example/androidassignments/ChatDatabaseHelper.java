dbd5e5ec7b7dc480123afe650eaca0cf95b77cb5
gregmurray <gd-murray@hotmail.com>
2019-11-03 22:06:27 -0500

    assignment 3 part 1 done

---
+++ app/src/main/java/com/example/androidassignments/ChatDatabaseHelper.java
@@ -0,0 +1,34 @@
+package com.example.androidassignments;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class ChatDatabaseHelper extends SQLiteOpenHelper {
+    private static final String ACTIVITY_NAME = "ChatDatabaseHelper";
+    private static final String DATABASE_NAME = "Message.db";
+    private static final int VERSION_NUM = 1;
+    public static final String KEY_ID = "id";
+    public static final String KEY_MESSAGE = "message";
+    public static final String TABLE_NAME = "chat";
+    public ChatDatabaseHelper(Context ctx) {
+        super(ctx, DATABASE_NAME, null, VERSION_NUM);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i(ACTIVITY_NAME, "Calling onCreate");
+        String sql = "CREATE TABLE " + TABLE_NAME + " ( " + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                KEY_MESSAGE + " VARCHAR(60));";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.i(ACTIVITY_NAME, "Calling onUpgrade");
+        String sql = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(sql);
+        this.onCreate(db);
+    }
+}
--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -1,11 +1,14 @@
 package com.example.androidassignments;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,30 +21,48 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class ChatWindow extends AppCompatActivity {
-
+    public final static String ACTIVITY_NAME = "ChatWindow";
     ArrayList<String> messages;
     ListView listView;
     Button sendButton;
     ChatAdapter messageAdapter;
+    ChatDatabaseHelper dbHelper;
+    SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
-
+        dbHelper = new ChatDatabaseHelper(this);
         listView = (ListView) findViewById(R.id.ListView);
         sendButton = (Button) findViewById(R.id.SendButton);
         final EditText messageField = (EditText) findViewById(R.id.EditMessageText);
 
         messages = new ArrayList<String>();
+        db = dbHelper.getWritableDatabase();
+        String query = "SELECT * from " + ChatDatabaseHelper.TABLE_NAME + " ORDER BY id ASC";
+        Cursor cursor = db.rawQuery(query, null);
+        String msg;
+        while (cursor.moveToNext()){
+            msg = cursor.getString(cursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE));
+            messages.add(msg);
+            Log.i(ACTIVITY_NAME, "SQL Message: " + msg);
+            Log.i(ACTIVITY_NAME, "Cursors Column Count: " + cursor.getColumnCount() );
 
+        }
+        cursor.close();
         messageAdapter =new ChatAdapter( this );
         listView.setAdapter (messageAdapter);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String message = messageField.getEditableText().toString();
+                Log.i(ACTIVITY_NAME, "Added Message: " + message);
+                ContentValues values = new ContentValues();
+                values.put(ChatDatabaseHelper.KEY_MESSAGE, message);
+                db.insert(ChatDatabaseHelper.TABLE_NAME, null, values);
                 messageAdapter.notifyDataSetChanged();
-                messages.add(messageField.getEditableText().toString());
+                messages.add(message);
                 messageField.setText("");
             }
         });
@@ -79,5 +100,11 @@ public class ChatWindow extends AppCompatActivity {
             message.setText(   getItem(position)  );
             return result;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
     }
 }
